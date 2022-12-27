cb1486d0c79e3195ec02a7b43b2e45244bd8251c
Hannah <hannahli6060@gmail.com>
2022-12-27 01:35:11 -0800

    newest section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./Navbar";
 function App() {
   return (
     <div className="App">
+    
       <section id="landing">
         <Navbar />
         <div className="landing-content">
@@ -15,6 +16,26 @@ function App() {
           </div>
           <img src={shopProfile} alt="" className="shop-profile-img"></img>
           <button className="landing-btn">Shop Now</button>
+        </div>
+      </section>
+
+      <section id="newest">
+        <h2>Newest</h2>
+        <div className="new-item">
+          <img src={shopProfile} alt="" />
+          <div className="new-item-content">
+            <div>
+              <h3>
+                Banana Cat <br /> Tote Bag
+              </h3>
+              <span>$15.99 CAD</span>
+            </div>
+            <button>View</button>
+          </div>
+        </div>
+        <div className="new-item-collection">
+          <img src={shopProfile} alt=""></img>
+          <button>View More</button>
         </div>
       </section>
     </div>
