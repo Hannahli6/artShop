2c1cadbb24914d0f07b29ab50405a012961f832b
Hannah <hannahli6060@gmail.com>
2022-12-31 20:53:01 -0800

    about me page mobile, more styling

--- src/Pages/AboutMePage.js/index.js
+++ src/Pages/AboutMePage.js/index.js
@@ -1,7 +1,8 @@
 import "./AboutMePage.css";
 import Navbar from "../../Navbar";
 import Footer from "../../Footer";
-import shopProfile from "../../assets/shopProfile.png"
+import shopProfile from "../../assets/shopProfile.png";
+import SocialMediaBtns from "../../SocialMediaBtns";
 
 const AboutMePage = () => {
   const lightPurple = "#CBD3FF";
@@ -9,13 +10,28 @@ const AboutMePage = () => {
   return (
     <div>
       <Navbar backgroundColor={lightPurple} />
-      <section>
-        <h2>Hello, I’m Hannah!</h2>
-        <h3>Digital Artist</h3>
-        <span>My name is Hannah and I'm a highschool student. I'm currently practicing coding! I like to watch anime, bake & draw on my free time!</span>
-        <img src={shopProfile} alt=""></img>
+      <section id="about-me">
+        <div className="about-me-desc">
+          <h2>Hello, I’m Hannah!</h2>
+          <h3>Digital Artist</h3>
+          <span>
+            My name is Hannah and I'm a highschool student. I'm currently
+            practicing coding! I like to watch anime, bake & draw on my free
+            time!
+          </span>
+          <img src={shopProfile} alt=""></img>
+        </div>
+        <SocialMediaBtns iconColor={darkerPurple} />
+        <span className="instagram-title">Follow Me on Instagram @artnhannah</span>
+        <div className="instagram-img-container">
+          <img src={shopProfile} alt=""></img>
+          <img src={shopProfile} alt=""></img>
+          <img src={shopProfile} alt=""></img>
+          <img src={shopProfile} alt=""></img>
+          <img src={shopProfile} alt=""></img>
+        </div>
       </section>
-      <Footer backgroundColor={lightPurple} iconColor={darkerPurple}/>
+      <Footer backgroundColor={lightPurple} iconColor={darkerPurple} />
     </div>
   );
 };
